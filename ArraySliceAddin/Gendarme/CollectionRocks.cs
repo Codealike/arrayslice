0920db35b20c2db62b77d4bfe225c665a3d1daf3
Federico <federico.lois@corvalius.com>
2014-10-04 13:09:34 -0300

    Added Gendarme code to ease the development.

---
+++ ArraySliceAddin/Gendarme/CollectionRocks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraySliceAddin.Fody.Gendarme
+{
+    // Here we keep non-Gendarme/Cecil related rocks
+    public static class CollectionRocks
+    {
+
+        /// <summary>
+        /// Checks if the list does not contain the item. If so the item is added.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="self">The list.</param>
+        /// <param name="item">The item to add.</param>
+        public static void AddIfNew<T>(this ICollection<T> self, T item)
+        {
+            if (self == null)
+                throw new ArgumentNullException("self");
+            if (item == null)
+                throw new ArgumentNullException("item");
+
+            if (!self.Contains(item))
+                self.Add(item);
+        }
+
+        public static void AddRangeIfNew<T>(this ICollection<T> self, IEnumerable<T> items)
+        {
+            if (self == null)
+                throw new ArgumentNullException("self");
+            if (items == null)
+                throw new ArgumentNullException("items");
+
+            foreach (T item in items)
+            {
+                if (!self.Contains(item))
+                    self.Add(item);
+            }
+        }
+    }
+
+    public static class SystemRocks
+    {
+
+        /// <summary>
+        /// Check if a Version is empty (all zeros).
+        /// </summary>
+        /// <param name="self">The Version to check</param>
+        /// <returns>True if empty, False otherwise.</returns>
+        public static bool IsEmpty(this Version self)
+        {
+            if (self == null)
+                return true;
+            return ((self.Major == 0) && (self.Minor == 0) && (self.Build <= 0) && (self.Revision <= 0));
+        }
+    }
+}
---
+++ ArraySliceAddin/Gendarme/PrimitiveReferences.cs
@@ -0,0 +1,48 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraySliceAddin.Fody.Gendarme
+{
+    /// <summary>
+    /// Provide an easy way to get TypeReference to primitive types without having
+    /// direct access to the mscorlib.dll assembly (any ModuleDefinition will do).
+    /// </summary>
+    public static class PrimitiveReferences
+    {
+
+        // To avoid memory allocations this is done in three stages
+        // 1. the references are cached at the first call and reused afterward (very cheap)
+        // 2. we look if the module already have the TypeReference (cheap)
+        // 3. at last we go thru the full Import (costly)
+
+        // TODO - extend to all primivites
+        static TypeReference single_ref;
+        static TypeReference double_ref;
+
+        static TypeReference GetReference(Type type, IMetadataTokenProvider metadata)
+        {
+            ModuleDefinition module = metadata.GetAssembly().MainModule;
+            TypeReference tr;
+            if (!module.TryGetTypeReference(type.FullName, out tr))
+                tr = module.Import(type);
+            return tr;
+        }
+
+        static public TypeReference GetDouble(IMetadataTokenProvider metadata)
+        {
+            if (double_ref == null)
+                double_ref = GetReference(typeof(double), metadata);
+            return double_ref;
+        }
+
+        static public TypeReference GetSingle(IMetadataTokenProvider metadata)
+        {
+            if (single_ref == null)
+                single_ref = GetReference(typeof(float), metadata);
+            return single_ref;
+        }
+    }
+}
