c95091bd898ac872c746c703c74817754ec8b7f1
Federico <federico.lois@corvalius.com>
2014-11-04 09:24:12 -0300

    Changed to version 0.4.0

--- ArraySlice.Fody/AssemblyInfo.cs
+++ ArraySlice.Fody/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyVersion("0.3.41")]
-[assembly: AssemblyFileVersion("0.3.41")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
