d3bcbf4e2979f3d298e7d231904496799bf09c30
Federico Andres Lois <federico.lois@corvalius.com>
2015-02-10 11:43:58 -0300

    Added Code Contracts Fixed install.ps1 deployment script.

--- ArraySlice.Fody/AssemblyInfo.cs
+++ ArraySlice.Fody/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyVersion("0.5.11")]
-[assembly: AssemblyFileVersion("0.5.11")]
+[assembly: AssemblyVersion("0.5.12")]
+[assembly: AssemblyFileVersion("0.5.12")]
--- NuGet/install.ps1
+++ NuGet/install.ps1
@@ -57,23 +57,6 @@ function Update-FodyConfig($addinName, $project)
     $xml.Save($fodyWeaversPath)
 }
 
-function Fix-ReferencesCopyLocal($package, $project)
-{
-    Write-Host "Fix-ReferencesCopyLocal $($package.Id)"
-    $asms = $package.AssemblyReferences | %{$_.Name}
- 
-    foreach ($reference in $project.Object.References)
-    {
-        if ($asms -contains $reference.Name + ".dll")
-        {
-            if($reference.CopyLocal -eq $true)
-            {
-                $reference.CopyLocal = $false;
-            }
-        }
-    }
-}
-
 function UnlockWeaversXml($project)
 {
     $fodyWeaversProjectItem = $project.ProjectItems.Item("FodyWeavers.xml");
@@ -90,5 +73,3 @@ UnlockWeaversXml($project)
 RemoveForceProjectLevelHack $project
 
 Update-FodyConfig $package.Id.Replace(".Fody", "") $project
-
-Fix-ReferencesCopyLocal $package $project
